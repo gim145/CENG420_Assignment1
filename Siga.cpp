2905c53b11a7b2692d07c2ec606f334314224758
Jeremy Brooks <jeremybrooks94@gmail.com>
2017-05-24 10:56:13 -0700

    Added Hueristic to computer move. Still will not checkmate on 2nd computer move

--- Siga.cpp
+++ Siga.cpp
@@ -90,12 +90,137 @@ if(pos[des]!=0 || pos[move]!=1){
 
 void Siga::computerMove()
 {
+    //Horizontal Checkmate Markers
+    bool checkmate_H1 = false;
+    bool checkmate_H2 = false;
+    bool checkmate_H3 = false;
+    //Vertical Checkmate Markers
+    bool checkmate_V1 = false;
+    bool checkmate_V2 = false;
+    bool checkmate_V3 = false;
+    //Diagonal Checkmate Marker
+    bool checkmate_DL = false;
+    bool checkmate_DR = false;
+
+    int move = 7;
+    int des = 0;
+
+/*
+/ First Move section will check if computer is 1 move away from win
+*/
+
+//Check Horizontal Line 1
+       if(pos[0]==2 && pos[1]==2) checkmate_H1 = true; des = 2;
+       if(pos[1]==2 && pos[2]==2) checkmate_H1 = true; des = 0;
+       if(pos[0]==2 && pos[2]==2) checkmate_H1 = true; des = 1;
+// If true initiate checkmate
+if(checkmate_H1){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=0 && i!=1 && i!=2) move = i;  
+    }
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+}
+
+//Check Horizontal Line 2
+       if(pos[3]==2 && pos[4]==2) checkmate_H2 = true; des = 5;
+       if(pos[4]==2 && pos[5]==2) checkmate_H2 = true; des = 3;
+       if(pos[3]==2 && pos[5]==2) checkmate_H2 = true; des = 4;
+// If true initiate checkmate
+if(checkmate_H2){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=3  && i!=4 && i!=5) move = i;  
+    }
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+}
+
+//Check Horizontal Line 3
+       if(pos[6]==2 && pos[7]==2) checkmate_H3 = true; des = 8;
+       if(pos[7]==2 && pos[8]==2) checkmate_H3 = true; des = 6;
+       if(pos[6]==2 && pos[8]==2) checkmate_H3 = true; des = 7;
+// If true initiate checkmate
+if(checkmate_H3 && totalTurns>2){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=6 && i!=7 && i!=8) move = i;  
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+    }
+}
+
+ // Check Vertical Column 1
+       if(pos[0]==2 && pos[3]==2) checkmate_V1 = true; des = 6;
+       if(pos[3]==2 && pos[6]==2) checkmate_V1 = true; des = 0;
+       if(pos[0]==2 && pos[6]==2) checkmate_V1 = true; des = 3;
+// If true initiate checkmate
+if(checkmate_V1){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=0  && i!=3 && i!=6) move = i;  
+    }
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+}
+ // Check Vertical Column 2
+       if(pos[1]==2 && pos[4]==2) checkmate_V2 = true; des = 7;
+       if(pos[4]==2 && pos[7]==2) checkmate_V2 = true; des = 1;
+       if(pos[1]==2 && pos[7]==2) checkmate_V2 = true; des = 4;
+// If true initiate checkmate
+if(checkmate_V2){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=1  && i!=4 && i!=7) move = i;  
+    }
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+}
+ // Check Vertical Column 3
+       if(pos[2]==2 && pos[8]==2) checkmate_V3 = true; des = 5;
+       if(pos[8]==2 && pos[5]==2) checkmate_V3 = true; des = 2;
+       if(pos[2]==2 && pos[5]==2) checkmate_V3 = true; des = 8;
+ // If true initiate checkmate
+if(checkmate_V3){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=2  && i!=5 && i!=8) move = i;  
+    }
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+}   
+
+// Check Diagonal
+   //Check Left Diagonal
+   if(pos[0] ==2 && pos[4]==2 ) checkmate_DL = true; move = 8;
+   if(pos[4] ==2 && pos[8]==2 ) checkmate_DL = true; move = 1;
+   if(pos[0] ==2 && pos[8]==2 ) checkmate_DL = true; move = 4;
+ // If true initiate checkmate
+if(checkmate_DL){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=0 && i!=4 && i!=8) des = i;  
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+    }
+}
+   //Check Right Diagonal
+   if(pos[2] ==2 && pos[4]==2 ) checkmate_DR = true; move = 6;
+   if(pos[4] ==2 && pos[6]==2 ) checkmate_DR = true; move = 2;
+   if(pos[2] ==2 && pos[6]==2 ) checkmate_DR = true; move = 4;
+ // If true initiate checkmate
+if(checkmate_DR){
+    for(int i =0; i<=8; i++){
+    if(pos[i]==2 && i!=2 && i!=4 && i!=6) des = i;  
+    pos[des] = 2;
+    pos[move] = 0;
+    return;
+    }
+}
 
 //Currently this code contains only a dummy computer opponent
 //Required to be improved to be implemented with uniformed search techniques
-    int move = 0;
-    int des =0;
-
     for(int i=0; i<=8; i++){
         if(pos[i] == 2){
             move = i;
@@ -106,9 +231,9 @@ void Siga::computerMove()
             des = i;
         }
 }
-
-    pos[des] = 2;
     pos[move] = 0;
+    pos[des] = 2;
+    
 }
 
 bool Siga::Winner()
